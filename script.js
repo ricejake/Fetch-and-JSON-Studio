d164bc54ef19524b853daaa5f708fb598eebb607
ricejake <ecirbocaj@yahoo.com>
2021-03-25 17:01:15 -0500

    finished

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-// TODO: add code here
+window.addEventListener('load', () => {
+  const json = fetch(
+    'https://handlers.education.launchcode.org/static/astronauts.json'
+    ).then((response) => 
+    response.json().then((resonseJsonArray) => {
+      console.log(resonseJsonArray);
+      const div = document.getElementById("container");
+      let webPage = '';
+      const buildAstronautHtml = (astronaut) => {
+        console.log(astronaut);
+        return`
+        <div class="astronaut">
+          <div class="bio">
+            <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+            <ul>
+              <li>Hours in space: ${astronaut.hoursInSpace}</li>
+              <li>Active: ${astronaut.active}</li>
+              <li>Skills: ${astronaut.skills}</li>
+            </ul>
+          </div> 
+          <img class="avatar" src=${astronaut.picture}>
+        </div> 
+        `
+        ;
+    } 
+    for(astronaut of resonseJsonArray) {
+      const newHtml = buildAstronautHtml(astronaut);
+      console.log(newHtml);
+      webPage += newHtml;
+    }
+    console.log('webpage', webPage);
+    div.innerHTML = webPage;
+  })
+  )
+})
+
